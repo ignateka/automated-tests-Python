1a3fa58fb290015178194c4b64edb01cc99450df
ignateka <elizaveta.ignat03@mail.ru>
2024-08-14 04:34:03 +0700

    first commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://8e2521f4-e61a-450a-ab2b-3781edab5ed9.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
